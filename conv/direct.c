9fd84ff5188c20d38358c5b65390cf4e78667133
letiankang <letiankang@tencent.com>
2020-09-16 16:32:14 +0800

    opt2

--- conv/direct.c
+++ conv/direct.c
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <immintrin.h>
 #include <sys/time.h>
 #include <stdio.h>
@@ -10,7 +11,7 @@
 #define SIMD_SIZE 16
 */
 
-const unsigned iN=64;
+const unsigned iN=32;
 const unsigned iC=256;
 const unsigned iH=28;
 const unsigned iW=28;
@@ -20,7 +21,7 @@ const unsigned wW=5;
 const unsigned wC=iC;
 const unsigned wD=32;
 
-const unsigned stride=1;
+const unsigned stride=2;
 
 const unsigned oN=iN;
 const unsigned oC=wD;
@@ -74,26 +75,30 @@ void Conv2D_opt2(float *In, float *We, float *Ou,
   for(i=0; i<iN; i++) { // input N/output N
     float* in_nptr = &In[i*iC*iH*iW]; 
     float* ou_nptr = &Ou[i*oC*oH*oW];
-    for(j=0; j<wD; j++) { // output C/weight D
-      float *ou_cptr = &ou_nptr[j*oH*oW];
-      float *we_dptr = &We[j];
-      for(k=0; k<iC; k++) { // input C/weight C
-        float *in_cptr = &in_nptr[k*iH*iW];
-        float *we_cptr = &we_dptr[k*wD];
-        for(l=0; l<oH; l++) { // output H
-          float *ou_hptr = &ou_cptr[l*oW];
-          float *in_hptr = &in_cptr[2*l*iW];
-          for(m=0; m<oW; m++) { // output W
-            float out = 0.;
-            float *in_win_ptr = &in_hptr[2*m];
-            for(n=0; n<wH; n++) { // weight H
-              float *we_hptr = &we_cptr[n*wW*wC*wD];
-              float *in_win_line_ptr = &in_win_ptr[n*iW];
-              for(o=0; o<wW; o++) { // weight W
-                out += we_hptr[o*wC*wD] * in_win_line_ptr[o];
+    for(n=0; n<wH; n++) { // weight H
+      float *we_hptr = &We[n*wW*wC*wD];
+      float *in_hptr = &in_nptr[n*iW];
+      for(o=0; o<wW; o++) { // weight W
+        float *we_wptr = &we_hptr[o*wC*wD];
+        float *in_wptr = &in_hptr[o];
+        for(k=0; k<iC; k++) { // input C/weight C
+          float *we_cptr = &we_wptr[k*wD];
+          float *in_cptr = &in_wptr[k*iH*iW];
+          float input_cache[oH*oW];
+          for (l=0;l<oH*oW;l++) {
+            in_cptr += stride;
+            input_cache[l] = *in_cptr;
+          }
+          for(j=0; j<wD; j++) { // output C/weight D
+            float *ou_cptr = &ou_nptr[j*oH*oW];
+            float weight = we_cptr[j];
+            for(l=0; l<oH; l++) { // output H
+              float *in_line_ptr = &input_cache[l*oW];
+              float *ou_hptr = &ou_cptr[l*oW];
+              for(m=0; m<oW; m++) { // output W
+                ou_hptr[m] += weight * in_line_ptr[m];
               }
             }
-            ou_hptr[m] = out;
           }
         }
       }
@@ -119,10 +124,10 @@ void Conv2D_org(float *In, float *We, float *Ou,
         float *we_cptr = &we_dptr[k*wD];
         for(l=0; l<oH; l++) { // output H
           float *ou_hptr = &ou_cptr[l*oW];
-          float *in_hptr = &in_cptr[2*l*iW];
+          float *in_hptr = &in_cptr[stride*l*iW];
           for(m=0; m<oW; m++) { // output W
             float out = 0.;
-            float *in_win_ptr = &in_hptr[2*m];
+            float *in_win_ptr = &in_hptr[stride*m];
             for(n=0; n<wH; n++) { // weight H
               float *we_hptr = &we_cptr[n*wW*wC*wD];
               float *in_win_line_ptr = &in_win_ptr[n*iW];
@@ -162,6 +167,12 @@ int main() {
 
   gettimeofday( &begin, NULL );
   Conv2D_opt(In, We, Ou, iN, iC, iH, iW, wH, wW, wC, wD, oN, oC, oH, oW);
+  gettimeofday( &end, NULL );
+  timeuse = (1000000. * ( end.tv_sec - begin.tv_sec ) + end.tv_usec - begin.tv_usec)/1000.;
+  printf("opt time: %.2f ms\n", timeuse);
+
+  gettimeofday( &begin, NULL );
+  Conv2D_opt2(In, We, Ou, iN, iC, iH, iW, wH, wW, wC, wD, oN, oC, oH, oW);
   gettimeofday( &end, NULL );
   timeuse = (1000000. * ( end.tv_sec - begin.tv_sec ) + end.tv_usec - begin.tv_usec)/1000.;
   printf("opt time: %.2f ms\n", timeuse);
